3914b93d875af1eba08866c002bbcc71388114a0
Robert Judson <robjudmello@hotmail.com>
2017-11-27 12:24:12 -0300

    Cópia do projeto V1

---
+++ Projeto_fpin_2017-2_V2/conecta.php
@@ -0,0 +1,29 @@
+<?php
+
+    $conn = new mysqli("localhost", "root", "", "wikilitica");
+
+    if($conn->connect_error){
+        echo "Error: " . $conn->connect_error;
+        exit;
+    }
+
+    // $statement = $conn->prepare("INSERT INTO usuario (nome, senha) VALUES(?, ?)");
+
+    // $statement->bind_param("ss", $login, $pass);
+
+    // $login = "Robert";
+    // $pass = "123456";
+
+    // $statement->execute();
+
+        $resultado = $conn->query("SELECT * FROM usuario ORDER BY idUsuario");
+
+        $data = array();
+
+        while ($row = $resultado->fetch_assoc()){
+            array_push($data, $row);
+        }
+
+        echo json_encode($data);
+
+?>
